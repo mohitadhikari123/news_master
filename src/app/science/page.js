e5389434eec4585858d58b5340b143996e02c9c9
mohitadhikari123 <111336786+mohitadhikari123@users.noreply.github.com>
2023-08-29 10:20:38 +0530

    Update api

--- src/app/science/page.js
+++ src/app/science/page.js
@@ -3,7 +3,7 @@ import Card from "../component/card";
  
 async function getData() {
   const res = await fetch(
-    "https://newsapi.org/v2/everything?q=science&apiKey=a9d13c06004e44b69d6d49aaa484f84e"
+        "https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=a9d13c06004e44b69d6d49aaa484f84e"  
   );
   return res.json();
 }
