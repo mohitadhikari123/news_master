e9313a27a321567e83d798a68dfc7739948eea7b
mohitadhikari123 <111336786+mohitadhikari123@users.noreply.github.com>
2024-07-20 18:33:24 +0530

    Update page.js

--- src/app/technology/page.js
+++ src/app/technology/page.js
@@ -1,11 +1,9 @@
 import styles from "../page.module.css";
 import Card from "../component/card";
-// "https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=93c3f039d28010095e57f092e3c32b8f"
-// https://gnews.io/api/v4/top-headlines?country=india&lang=en&category=technology&apikey=93c3f039d28010095e57f092e3c32b8f
 
 async function getData() {
   const res = await fetch(
-    "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=a9d13c06004e44b69d6d49aaa484f84e"
+    "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=a9d13c06004e44b69d6d49aaa484f84e"
   );
   return res.json();
 }
